058fcb79df6664e6887f26a61bfca5dcdea6cf22
Jenny Martin <jennymartin@Jennys-MBP.lan1>
2019-08-23 00:08:05 -0500

    updated addition and subtraction functionality

--- src/App.js
+++ src/App.js
@@ -8,21 +8,38 @@ class App extends Component {
     this.state = {
       count: 0
     }
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleSubtract = this.handleSubtract.bind(this)
   }
   
-  handleClick() {
-    this.setState()
-    console.log("It works!")
-  }
+  handleAdd() {
+    this.setState((prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  )
+}
+
+handleSubtract() {
+  this.setState((prevState => {
+    return {
+      count: prevState.count - 1
+    }
+  })
+)
+}
 
   render() {
-  return (
-    <div className="counter">
-      <h1>{this.state.count}</h1>
-      <button onClick={this.handleClick}>CHANGE!</button>
-    </div>
-  );
-}
+    return (
+      <div className="counter">
+        <h3>Click on the button to add or subtract by 1.</h3>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleAdd}>Add me!</button>
+        <button onClick={this.handleSubtract}>Subtract me!</button>
+      </div>
+    )
+  }
 }
 
 export default App;
